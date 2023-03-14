da60a28dd898470d29a4cac90604508d46d2c01a
Swapnil <swapnilmahadule0597@gmail.com>
2023-03-14 09:27:53 +0530

    1st commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,26 @@
 package com.driver;
 
 public class Main {
+    static class Product{
+        public static int product(int x, int y) {
+            return x * y;
+        }
+        public int product(int x, int y, int z) {
+            return x * y * z;
+        }
+        public double product(double x, double y) {
+            return x * y;
+        }
+    }
 
+    public static void main(String[] args) {
+
+        Product p = new Product();
+
+        int ans1 = p.product(10, 20);
+        int ans2 = p.product(10 , 20 , 30);
+        double ans3 = p.product(10,30);
+
+
+    }
 }
